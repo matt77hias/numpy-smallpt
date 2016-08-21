6c9c1163eaf4a49f9fd85456326a5c949d640eed
ALIEN <matthias.moulin@gmail.com>
2016-08-21 18:36:02 +0200

    130 instead of 140 shift

--- numpy-smallpt/core/numpy_smallpt.py
+++ numpy-smallpt/core/numpy_smallpt.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
                         dy = np.sqrt(u2) - 1.0 if u2 < 1 else 1.0 - np.sqrt(2.0 - u2)
                         d = cx * (((sx + 0.5 + dx) / 2.0 + x) / w - 0.5) + \
                             cy * (((sy + 0.5 + dy) / 2.0 + y) / h - 0.5) + gaze
-                        L += radiance(Ray(eye + d * 140, normalize(d), tmin=Sphere.EPSILON_SPHERE), rng) * (1.0 / nb_samples)
+                        L += radiance(Ray(eye + d * 130, normalize(d), tmin=Sphere.EPSILON_SPHERE), rng) * (1.0 / nb_samples)
                     Ls[i,:] += 0.25 * np.clip(L, a_min=0.0, a_max=1.0)
 
     write_ppm(w, h, Ls)
