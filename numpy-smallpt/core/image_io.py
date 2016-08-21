32c48f587d9ed3484fe4097d9848bacef9bb333f
ALIEN <matthias.moulin@gmail.com>
2016-08-21 09:07:10 +0200

    Changed default fname

--- numpy-smallpt/core/image_io.py
+++ numpy-smallpt/core/image_io.py
@@ -1,6 +1,6 @@
 from math_tools import to_byte
 
-def write_ppm(w, h, Ls, fname = "image.ppm"):
+def write_ppm(w, h, Ls, fname = "numpy-image.ppm"):
     with open(fname, 'w') as outfile:
         outfile.write('P3\n{0} {1}\n{2}\n'.format(w, h, 255));
         for i in range(Ls.shape[0]):
